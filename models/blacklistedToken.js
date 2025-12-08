e5745465001f610d028bb38813b48d51e0bd9c53
Daniel <daniel.chassagn@gmail.com>
2025-12-08 11:14:44 +0100

    add blackListedToken mongoose schema

---
+++ models/blacklistedToken.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const blacklistedTokenSchema = new mongoose.Schema({
+    tokenHash: String,
+    createdAt: {
+        type: Date,
+        expires: "7d",
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model("BlacklistedToken", blacklistedTokenSchema);    
